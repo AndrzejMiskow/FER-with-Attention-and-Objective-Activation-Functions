daaa12c1e73074eb44cc1dd6ddb3e414ecc99ee7
Andrzej Miskow <sc19am2@leeds.ac.uk>
2022-02-22 17:13:41 +0000

    Modified detect_face file to extract a cropped face from an image and removed command line arguments so the program can be ran from a diffrent python program

--- detect_face.py
+++ detect_face.py
@@ -1,4 +1,5 @@
-# -*- coding: UTF-8 -*-
+# Modifed version of detect_face file from yolo5-face project
+
 import argparse
 import time
 from pathlib import Path
@@ -8,6 +9,9 @@ import torch
 import torch.backends.cudnn as cudnn
 from numpy import random
 import copy
+import sys
+
+sys.path.append("utils/yolo5-face")
 
 from models.experimental import attempt_load
 from utils.datasets import letterbox
@@ -34,7 +38,7 @@ def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
     coords[:, [0, 2, 4, 6, 8]] -= pad[0]  # x padding
     coords[:, [1, 3, 5, 7, 9]] -= pad[1]  # y padding
     coords[:, :10] /= gain
-    #clip_coords(coords, img0_shape)
+    # clip_coords(coords, img0_shape)
     coords[:, 0].clamp_(0, img0_shape[1])  # x1
     coords[:, 1].clamp_(0, img0_shape[0])  # y1
     coords[:, 2].clamp_(0, img0_shape[1])  # x2
@@ -47,32 +51,42 @@ def scale_coords_landmarks(img1_shape, coords, img0_shape, ratio_pad=None):
     coords[:, 9].clamp_(0, img0_shape[0])  # y5
     return coords
 
-def show_results(img, xywh, conf, landmarks, class_num):
-    h,w,c = img.shape
+
+def show_results(img, xywh, conf, landmarks, class_num, face_size):
+    h, w, c = img.shape
     tl = 1 or round(0.002 * (h + w) / 2) + 1  # line/font thickness
+
+    # define the region of the face in the image
     x1 = int(xywh[0] * w - 0.5 * xywh[2] * w)
     y1 = int(xywh[1] * h - 0.5 * xywh[3] * h)
     x2 = int(xywh[0] * w + 0.5 * xywh[2] * w)
     y2 = int(xywh[1] * h + 0.5 * xywh[3] * h)
-    cv2.rectangle(img, (x1,y1), (x2, y2), (0,255,0), thickness=tl, lineType=cv2.LINE_AA)
 
-    clors = [(255,0,0),(0,255,0),(0,0,255),(255,255,0),(0,255,255)]
+    # Cropping the face out of the original image and resizing it to keep consistency
+    dim = (face_size, face_size)
+    croped_face = img[y1:y2, x1:x2]
+    croped_face = cv2.resize(croped_face, dim, interpolation=cv2.INTER_AREA)
 
-    for i in range(5):
-        point_x = int(landmarks[2 * i] * w)
-        point_y = int(landmarks[2 * i + 1] * h)
-        cv2.circle(img, (point_x, point_y), tl+1, clors[i], -1)
+    # No need to draw boxes on croped faces, landmark feature may be used later on
 
-    tf = max(tl - 1, 1)  # font thickness
-    label = str(conf)[:5]
-    cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
-    return img
+    # cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), thickness=tl, lineType=cv2.LINE_AA)
+    # clors = [(255, 0, 0), (0, 255, 0), (0, 0, 255), (255, 255, 0), (0, 255, 255)]
+    #
+    # # Landmarks of the face unnecessary but may be useful for training the neural network
+    # for i in range(5):
+    #     point_x = int(landmarks[2 * i] * w)
+    #     point_y = int(landmarks[2 * i + 1] * h)
+    #     cv2.circle(img, (point_x, point_y), tl + 1, clors[i], -1)
+    # tf = max(tl - 1, 1)  # font thickness
+    # label = str(conf)[:5]  # confidence rating
+    # cv2.putText(img, label, (x1, y1 - 2), 0, tl / 3, [225, 255, 255], thickness=tf, lineType=cv2.LINE_AA)
+    #
+    return img, croped_face
 
 
-
-def detect_one(model, image_path, device):
-    # Load model
+def detect_one(model, image_path, device, face_size, output_path):
     img_size = 800
+    # Load model
     conf_thres = 0.3
     iou_thres = 0.5
 
@@ -82,7 +96,7 @@ def detect_one(model, image_path, device):
     h0, w0 = orgimg.shape[:2]  # orig hw
     r = img_size / max(h0, w0)  # resize image to img_size
     if r != 1:  # always resize down, only resize up if training with augmentation
-        interp = cv2.INTER_AREA if r < 1  else cv2.INTER_LINEAR
+        interp = cv2.INTER_AREA if r < 1 else cv2.INTER_LINEAR
         img0 = cv2.resize(img0, (int(w0 * r), int(h0 * r)), interpolation=interp)
 
     imgsz = check_img_size(img_size, s=model.stride.max())  # check img_size
@@ -129,20 +143,23 @@ def detect_one(model, image_path, device):
                 conf = det[j, 4].cpu().numpy()
                 landmarks = (det[j, 5:15].view(1, 10) / gn_lks).view(-1).tolist()
                 class_num = det[j, 15].cpu().numpy()
-                orgimg = show_results(orgimg, xywh, conf, landmarks, class_num)
 
+                # Output for each face box
+                orgimg, cropped_face = show_results(orgimg, xywh, conf, landmarks, class_num, face_size)
+                cv2.imwrite(output_path, cropped_face)
+
+    # test a single output
     cv2.imwrite('result.jpg', orgimg)
+    cv2.imwrite('cropped.jpg', cropped_face)
 
 
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str, default='runs/train/exp5/weights/last.pt', help='model.pt path(s)')
-    parser.add_argument('--image', type=str, default='data/images/test.jpg', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    opt = parser.parse_args()
-    print(opt)
+# Only need to load the model once (speeds up computation)
+def import_model(weights):
+    global device
+    global model
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model = load_model(opt.weights, device)
-    detect_one(model, opt.image, device)
+    model = load_model(weights, device)
+
+
+def run_detect_face(img_path, img_size, output_path):
+    detect_one(model, img_path, device, img_size, output_path)

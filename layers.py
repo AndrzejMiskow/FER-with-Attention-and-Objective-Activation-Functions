62fe76bc9a501ce3aee7293138a311bc3a09bd79
Andrzej Miskow <sc19am2@leeds.ac.uk>
2022-03-28 22:53:37 +0100

    Fixed batch normalisation in resnet v1 and v2, fixed implementation of resnet v2

--- layers.py
+++ layers.py
@@ -3,7 +3,7 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPool2D, Flatten, Dense, Ba
 
 
 def residual_block_v1(x, filters, kernel_size=3, stride=1, conv_shortcut=True, name=None, grayscale=True):
-    """A residual block for ResNetV1
+	"""A residual block for ResNetV1
     Args:
       x: input tensor.
       filters: array, filters of the bottleneck layer.
@@ -15,59 +15,119 @@ def residual_block_v1(x, filters, kernel_size=3, stride=1, conv_shortcut=True, n
     Returns:
       Output tensor for the residual block.
     """
-    if grayscale:
-        batch_axis = 1
-    else:
-        batch_axis = 3
+	batch_axis = 1
+	filters1, filters2, filters3 = filters
 
-    filters1, filters2, filters3 = filters
+	if conv_shortcut:
+		shortcut = layers.Conv2D(
+			filters3, 1, strides=stride, name=name + '_Shortcut_Conv')(x)
+		shortcut = layers.BatchNormalization(
+			axis=batch_axis, epsilon=1.001e-5, name=name + '_Shortcut_BN')(shortcut)
+	else:
+		shortcut = x
 
-    if conv_shortcut:
-        shortcut = layers.Conv2D(
-            filters3, 1, strides=stride, name=name + '_0_conv')(x)
-        shortcut = layers.BatchNormalization(
-            axis=batch_axis, epsilon=1.001e-5, name=name + '_0_bn')(shortcut)
-    else:
-        shortcut = x
+	x = layers.Conv2D(filters1, 1, strides=stride, name=name + '_1_Conv')(x)
+	x = layers.BatchNormalization(
+		axis=batch_axis, epsilon=1.001e-5, name=name + '_1_BN')(x)
+	x = layers.Activation('relu', name=name + '_1_Relu')(x)
 
-    x = layers.Conv2D(filters1, 1, strides=stride, name=name + '_1_conv')(x)
-    x = layers.BatchNormalization(
-        axis=batch_axis, epsilon=1.001e-5, name=name + '_1_bn')(x)
-    x = layers.Activation('relu', name=name + '_1_relu')(x)
+	x = layers.Conv2D(
+		filters2, kernel_size, padding='SAME', name=name + '_2_Conv')(x)
+	x = layers.BatchNormalization(
+		axis=batch_axis, epsilon=1.001e-5, name=name + '_2_BN')(x)
+	x = layers.Activation('relu', name=name + '_2_Relu')(x)
 
-    x = layers.Conv2D(
-        filters2, kernel_size, padding='SAME', name=name + '_2_conv')(x)
-    x = layers.BatchNormalization(
-        axis=batch_axis, epsilon=1.001e-5, name=name + '_2_bn')(x)
-    x = layers.Activation('relu', name=name + '_2_relu')(x)
+	x = layers.Conv2D(filters3, 1, name=name + '_3_Conv')(x)
+	x = layers.BatchNormalization(
+		axis=batch_axis, epsilon=1.001e-5, name=name + '_3_BN')(x)
 
-    x = layers.Conv2D(filters3, 1, name=name + '_3_conv')(x)
-    x = layers.BatchNormalization(
-        axis=batch_axis, epsilon=1.001e-5, name=name + '_3_bn')(x)
+	x = layers.Add(name=name + '_Add')([shortcut, x])
+	x = layers.Activation('relu', name=name + '_Output')(x)
 
-    x = layers.Add(name=name + '_add')([shortcut, x])
-    x = layers.Activation('relu', name=name + '_out')(x)
-
-    return x
+	return x
 
 
-def group_residuals_v1(x, filters, blocks, stride1=2, name=None, grayscale=True):
-    """A group of stacked residual blocks for ResNetV1
+def group_residuals_v1(x, filters, blocks, stride1=2, name=None):
+	"""A group of stacked residual blocks for ResNetV1
     Args:
       x: tensor, input
       filters: array, filters of the bottleneck layer in a block.
       blocks: integer, blocks in the stacked blocks.
       stride1: default 2, stride of the first layer in the first block.
       name: string, group of blocks label.
-      grayscale: bool, states when the input tensor is RGB or Grayscale
     Returns:
       Output tensor for the stacked blocks.
     """
 
-    x = residual_block_v1(x, filters, stride=stride1, name=name + '_Block1', grayscale=grayscale)
+	x = residual_block_v1(x, filters, stride=stride1, name=name + '_Block1')
 
-    for i in range(2, blocks + 1):
-        x = residual_block_v1(x, filters, conv_shortcut=False, name=name + '_Block' + str(i), grayscale=grayscale)
+	for i in range(2, blocks + 1):
+		x = residual_block_v1(x, filters, conv_shortcut=False, name=name + '_Block' + str(i))
+	return x
 
-    return x
 
+def residual_block_v2(x, filters, kernel_size=3, stride=1, conv_shortcut=False, name=None):
+	"""A residual block for ResNetV2
+    Args:
+      x: input tensor.
+      filters: array, filters of the bottleneck layer.
+      kernel_size: default 3, kernel size of the bottleneck layer.
+      stride: default 1, stride of the first layer.
+      conv_shortcut: default True, use convolution shortcut if True, otherwise identity shortcut.
+      name: string, block label.
+    Returns:
+      Output tensor for the residual block.
+    """
+
+	batch_axis = 1
+
+	filters1, filters2, filters3 = filters
+
+	# ResNetV2 uses pre-activation which makes it perform better on deeper networks
+	pre_activation = layers.BatchNormalization(
+		axis=batch_axis, epsilon=1.001e-5, name=name + '_Pre-Activation_BN')(x)
+	pre_activation = layers.Activation('relu', name=name + '_Pre-Activation_Relu')(pre_activation)
+
+	if conv_shortcut:
+		shortcut = layers.Conv2D(
+			filters3, 1, strides=stride, name=name + '_Shortcut_Conv')(pre_activation)
+	else:
+		shortcut = layers.MaxPooling2D(1, strides=stride)(x) if stride > 1 else x
+
+	x = layers.Conv2D(filters1, 1, strides=1, use_bias=False,
+					  name=name + '_1_Conv')(pre_activation)
+	x = layers.BatchNormalization(
+		axis=batch_axis, epsilon=1.001e-5, name=name + '_1_BN')(x)
+	x = layers.Activation('relu', name=name + '_1_Relu')(x)
+
+	x = layers.ZeroPadding2D(padding=((1, 1), (1, 1)), name=name + '_2_Pad')(x)
+	x = layers.Conv2D(filters2, kernel_size, strides=stride,
+					  use_bias=False, name=name + '_2_Conv')(x)
+	x = layers.BatchNormalization(
+		axis=batch_axis, epsilon=1.001e-5, name=name + '_2_BN')(x)
+	x = layers.Activation('relu', name=name + '_2_Relu')(x)
+
+	x = layers.Conv2D(filters3, 1, name=name + '_3_Conv')(x)
+	x = layers.Add(name=name + '_Add_Output')([shortcut, x])
+
+	return x
+
+
+def group_residuals_v2(x, filters, blocks, stride1=2, name=None):
+	"""A group of stacked residual blocks for ResNetV2
+    Args:
+      x: tensor, input
+      filters: array, filters of the bottleneck layer in a block.
+      blocks: integer, blocks in the stacked blocks.
+      stride1: default 2, stride of the first layer in the first block.
+      name: string, group of blocks label.
+    Returns:
+      Output tensor for the stacked blocks.
+    """
+
+	x = residual_block_v2(x, filters, conv_shortcut=True, name=name + '_Block1')
+	for i in range(2, blocks):
+		x = residual_block_v2(x, filters, name=name + '_Block' + str(i))
+	# stride is changed here due to the pre-activation in the next layer
+	x = residual_block_v2(x, filters, stride=stride1, name=name + '_Block' + str(blocks))
+	return x
